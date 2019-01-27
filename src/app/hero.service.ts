d5de6db69b9f29de6c86c96c3556955b6a92f672
derui <derutakayu@gmail.com>
2019-01-27 09:22:59 +0900

    Initial commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
+
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {MessageService} from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero>{
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    // TODO: implement with state management
+    return of();
+  }
+
+  addHero(hero: Hero): Observable<any> {
+    // TODO: implement with state management
+    return of();
+  }
+
+  deleteHero(hero: Hero): Observable<any> {
+    // TODO: implement with state management
+    return of();
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    // TODO: implement with state management
+    return of([]);
+  }
+}
